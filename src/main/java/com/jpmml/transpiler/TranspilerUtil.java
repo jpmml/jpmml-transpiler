a391bddddaa34e42c8dbe48103fe3f015c57545e
Villu Ruusmann <villu.ruusmann@gmail.com>
2019-11-04 14:50:20 +0200

    Updated header

--- src/main/java/com/jpmml/transpiler/TranspilerUtil.java
+++ src/main/java/com/jpmml/transpiler/TranspilerUtil.java
@@ -86,14 +86,29 @@ public class TranspilerUtil {
 		String[] lines = {
 			"/*",
 			" * Copyright (c) 2019 Villu Ruusmann",
+			" *",
+			// All the generated code is functionally dependent on the JPMML-Evaluator library.
+			" * This file is part of JPMML-Evaluator",
+			" *",
+			" * JPMML-Evaluator is free software: you can redistribute it and/or modify",
+			" * it under the terms of the GNU Affero General Public License as published by",
+			" * the Free Software Foundation, either version 3 of the License, or",
+			" * (at your option) any later version.",
+			" *",
+			" * JPMML-Evaluator is distributed in the hope that it will be useful,",
+			" * but WITHOUT ANY WARRANTY; without even the implied warranty of",
+			" * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the",
+			" * GNU Affero General Public License for more details.",
+			" *",
+			" * You should have received a copy of the GNU Affero General Public License",
+			" * along with JPMML-Evaluator.  If not, see <http://www.gnu.org/licenses/>.",
 			" */"
 		};
-		String lineSeparator = System.getProperty("line.separator");
 
 		StringBuilder sb = new StringBuilder();
 
 		for(String line : lines){
-			sb.append(line).append(lineSeparator);
+			sb.append(line).append(System.lineSeparator());
 		}
 
 		HEADER = sb.toString();
